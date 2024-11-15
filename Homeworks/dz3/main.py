e01d68affb65193b357618109996dc6cd45ad824
Wh1teMag1c <dmitrybwm@yandex.ru>
2024-11-15 11:25:42 +0300

    update main.py

--- Homeworks/dz3/main.py
+++ Homeworks/dz3/main.py
@@ -5,22 +5,16 @@ import sys
 
 def parse_xml_to_config(xml_input):
     try:
-        # Парсим XML из входной строки
         root = ET.fromstring(xml_input)
         return convert_element_to_config(root)
     except ET.ParseError as e:
-        # Создаем новое исключение с сообщением, которое мы хотим
         raise SyntaxError("Ошибка синтаксиса XML: Недопустимое имя тега") from e
 
 
 def convert_element_to_config(element, indent=0):
-    """
-    Рекурсивно конвертирует элемент XML в конфигурационный язык.
-    """
     config_output = ""
-    indent_str = "    " * indent  # 4 пробела для каждого уровня вложенности
+    indent_str = "    " * indent
 
-    # Проверка на комментарии
     if element.tag.lower() == "comment":
         if "multiline" in element.attrib and element.attrib["multiline"] == "true":
             lines = element.text.strip().split('\n')
@@ -30,7 +24,6 @@ def convert_element_to_config(element, indent=0):
             config_output += f"{indent_str}# {element.text.strip()}\n"
         return config_output
 
-    # Конвертация элемента в словарь
     config_output += f"{indent_str}{{\n"
     for child in element:
         if re.match(r"[_a-zA-Z]+", child.tag):
@@ -49,20 +42,13 @@ def convert_element_to_config(element, indent=0):
 
 
 def parse_value(element):
-    """
-    Определяет значение для элемента.
-    """
-    # Если у элемента есть дочерние элементы, то это вложенный словарь
     if len(element):
         return f"\n{convert_element_to_config(element, indent=1)}"
 
-    # Если значение - это число
     if element.text and element.text.strip().isdigit():
         return element.text.strip()
 
-    # Обработка текстовых значений
     if element.text and element.text.strip():
-        # Возвращаем строку в кавычках
         return f'"{element.text.strip()}"'
 
     raise SyntaxError(f"Не удалось определить значение для элемента {element.tag}")
@@ -74,7 +60,6 @@ if __name__ == "__main__":
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             xml_input = file.read()
-
         config_output = parse_xml_to_config(xml_input.strip())
         print("\nРезультат:")
         print(config_output)
